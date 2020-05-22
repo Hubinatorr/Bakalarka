b5a85e9f589fc621716193c69f621ca538795eb5
Hubinatorr <robert.hubinak@gmail.com>
2020-05-22 12:50:10 +0200

    Revert "panel a zlepsena navigacia"
    This reverts commit f92205b32d3b08fee1ac42ce09545aaa9fb79436.

--- Assets/MissionWaypoint.cs
+++ Assets/MissionWaypoint.cs
@@ -20,14 +20,14 @@ public class MissionWaypoint : MonoBehaviour
         // Giving limits to the icon so it sticks on the screen
         // Below calculations witht the assumption that the icon anchor point is in the middle
         // Minimum X position: half of the icon width
-        float minX = img.GetPixelAdjustedRect().width / 2+1;
+        float minX = img.GetPixelAdjustedRect().width / 2;
         // Maximum X position: screen width - half of the icon width
-        float maxX = Screen.width*0.81f - minX-1;
+        float maxX = Screen.width - minX;
 
         // Minimum Y position: half of the height
-        float minY = Screen.height*0.08f + img.GetPixelAdjustedRect().height / 2 +1;
+        float minY = img.GetPixelAdjustedRect().height / 2;
         // Maximum Y position: screen height - half of the icon height
-        float maxY = Screen.height*0.96f - img.GetPixelAdjustedRect().height / 2 -2;
+        float maxY = Screen.height - minY;
 
         // Temporary variable to store the converted position from 3D world point to 2D screen point
         Vector2 pos = cam.WorldToScreenPoint(target.position + offset);
@@ -47,6 +47,7 @@ public class MissionWaypoint : MonoBehaviour
                 pos.x = minX;
             }
         }
+
         // Limit the X and Y positions
         pos.x = Mathf.Clamp(pos.x, minX, maxX);
         pos.y = Mathf.Clamp(pos.y, minY, maxY);
